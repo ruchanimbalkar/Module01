17736b8a8a91d1258bcac736f89b05874eb34e80
Rucha Nimbalkar <16888353+ruchanimbalkar@users.noreply.github.com>
2024-12-22 21:42:42 -0800

    comments in python

---
+++ comments.py
@@ -0,0 +1,26 @@
+#-------------------------------------------------------------------------------------------------
+# Title: Comments in Python
+# Description: Basics of Python (comments)
+# Change Log: (Who, When, What)
+# RNimbalkar, 12/22/2024 , Created Script
+#--------------------------------------------------------------------------------------------------
+
+#This is a one line comment
+
+'''
+This is a multi-line
+comment
+in
+Python.
+It is also
+known as a
+block comment.
+'''
+
+
+"""
+Using meaningful comments throughout your code helps improve code 
+readability, maintainability, and collaboration among developers.
+It allows others (including yourself) to understand the code's intent,
+making it easier to debug, modify, and maintain the codebase.
+"""
